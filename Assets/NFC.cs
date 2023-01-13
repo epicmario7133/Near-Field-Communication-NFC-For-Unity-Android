76ee2b5b34c9b7cbbf371b3b1a1138f14c1fec8f
EpicMario71 <leonardaeadi@gmail.com>
2023-01-13 23:23:19 +0100

    Fixed and upgraded to Unity2019.4.31f1

--- Assets/NFC.cs
+++ Assets/NFC.cs
@@ -28,26 +28,15 @@ public class NFC : MonoBehaviour {
 					mActivity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity"); // Activities open apps
 					mIntent = mActivity.Call<AndroidJavaObject>("getIntent");
 					sAction = mIntent.Call<String>("getAction"); // resulte are returned in the Intent object
-					if (sAction == "android.nfc.action.NDEF_DISCOVERED") {
+					if (sAction == "android.nfc.action.NDEF_DISCOVERED")
+					{
 						Debug.Log("Tag of type NDEF");
-					}
-					else if (sAction == "android.nfc.action.TECH_DISCOVERED") {
-						Debug.Log("TAG DISCOVERED");
-						// Get ID of tag
-						AndroidJavaObject mNdefMessage = mIntent.Call<AndroidJavaObject>("getParcelableExtra", "android.nfc.extra.TAG");
-						if (mNdefMessage != null) {
-							byte[] payLoad = mNdefMessage.Call<byte[]>("getId");
-							string text = System.Convert.ToBase64String(payLoad);
-							tag_output_text.text += "This is your tag text: " + text; 
-							Destroy (GetComponent("MeshRenderer")); //Destroy Box when NFC ID is displayed
-							tagID = text;
-						}
-						else {
-							tag_output_text.text = "No ID found !";
-						}
-						tagFound = true;
-						// How to read multiple tags maybe with this line mIntent.Call("removeExtra", "android.nfc.extra.TAG");
-						return;
+						AndroidJavaObject[] rawMsg = mIntent.Call<AndroidJavaObject[]>("getParcelableArrayExtra", "android.nfc.extra.NDEF_MESSAGES");
+						AndroidJavaObject[] records = rawMsg[0].Call<AndroidJavaObject[]>("getRecords");
+						byte[] payLoad = records[0].Call<byte[]>("getPayload");
+						string result = System.Text.Encoding.Default.GetString(payLoad); // not sure if it works for all encodings, but it works for me
+						result = result.Replace("en", ""); //rimuovi EN
+						tag_output_text.text = result; // first few letters are about used language (for english "en..")
 					}
 					else if (sAction == "android.nfc.action.TAG_DISCOVERED") {
 						Debug.Log("This type of tag is not supported !");
